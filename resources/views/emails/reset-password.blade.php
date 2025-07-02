a1989400407978c2a0638f881cbd9b2b2234a510
Mahendra Varma <mahendra.varma22@gmail.com>
2025-07-03 00:01:26 +0530

    mail sending.

--- resources/views/emails/reset-password.blade.php
+++ resources/views/emails/reset-password.blade.php
@@ -2,13 +2,44 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Password Reset</title>
+    <title>Your Temporary Password for TASK365 Vendor Account</title>
 </head>
-<body>
-    <p>Dear {{ $user->name ?? 'User' }},</p>
-    <p>Your new password is: <strong>{{ $password }}</strong></p>
-    <p>You can log in using this password. For security, please update your password from your profile page after logging in.</p>
+<body style="font-family: Arial, sans-serif; color: #333; line-height: 1.6;">
+    <p>Dear Vendor,</p>
+
+    <p>You have requested to reset the password for your <strong>TASK365 vendor account</strong>.</p>
+
+    <p><strong>Here is your temporary password:</strong><br>
+    <strong>{{ $password }}</strong> <em>(Please change after login)</em></p>
+
+    <p><strong>Login Here:</strong> <a href="https://www.task365.in" target="_blank">www.task365.in</a></p>
+
+    <p>For your security, we strongly recommend updating your password immediately after logging in.</p>
+
+    <p>If you did not request this, please ignore this email or contact our support team right away.</p>
+
+    <p>For any help, feel free to write to us at <a href="mailto:support@task365.in">support@task365.in</a>.</p>
+
     <br>
-    <p>Thanks,<br>Task365 Team</p>
+
+    <p>Warm regards,<br>
+    Team TASK365<br>
+    <em>(A Company of Lorhan Spot Earn Pvt Ltd)</em><br>
+    <a href="https://www.task365.in" target="_blank">www.task365.in</a></p>
+
+    <hr style="margin: 40px 0; border: none; border-top: 1px solid #ccc;">
+
+    <small style="font-size: 12px; color: #666;">
+        <strong>Disclaimer:</strong><br>
+        This email and any files transmitted with it are confidential and intended solely for the use of the individual or entity to whom they are addressed. If you have received this email in error, please notify the sender immediately and delete it from your system. Any unauthorized review, use, disclosure, copying, distribution, or taking action based on its contents is strictly prohibited and may be unlawful.<br><br>
+
+        TASK365 (a product of Lorhan Spot Earn Pvt Ltd) accepts no liability for the content of this email or for any damage caused by any virus or malware transmitted by this email.<br><br>
+
+        All email communications are subject to monitoring by TASK365 in accordance with applicable laws. The views or opinions presented in this email do not necessarily represent those of the company unless explicitly stated.<br><br>
+
+        For support or further information, please contact us at <a href="mailto:support@task365.in">support@task365.in</a> or visit <a href="https://www.task365.in" target="_blank">www.task365.in</a>.<br><br>
+
+        This communication and any dispute arising from it are subject to Indian laws and the jurisdiction of courts located in Hyderabad, Telangana, India.
+    </small>
 </body>
 </html>
