a68ecea3d95df82cee5ebf42a64776e2fd5cff12
Mahendra Varma <mahendra.varma22@gmail.com>
2025-11-16 22:59:48 +0530

    commented div for comapnies display.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -318,7 +318,7 @@
 <!-- -------------img-slider End---------------------- -->
 
 <!-- ------------------------compnay logo slide start--------------------------------------- -->
-<div class="video-modal-area style-1 bg-white brand_sec">
+<!--<div class="video-modal-area style-1 bg-white brand_sec">
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -334,7 +334,7 @@
             </div>
         </div>
     </div>
-</div>
+</div>-->
 </div>
 
 
